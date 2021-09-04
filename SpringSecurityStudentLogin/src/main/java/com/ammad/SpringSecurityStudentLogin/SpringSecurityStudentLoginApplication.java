361974bb483931c3750536863b65a61fbd91e847
Acer <Acer@DESKTOP-7NUNTTE.mshome.net>
2021-09-04 17:16:41 +0530

    First Commit

---
+++ SpringSecurityStudentLogin/src/main/java/com/ammad/SpringSecurityStudentLogin/SpringSecurityStudentLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ammad.SpringSecurityStudentLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStudentLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityStudentLoginApplication.class, args);
+	}
+
+}
